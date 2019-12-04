b97097680c93ea674d7b655a5215741bf7d0e702
Eric Smasal <eric.smasal@syntak.com>
2019-12-04 15:54:35 -0600

    Initial Commit

--- Apothecaric.Authentication.Service/Startup.cs
+++ Apothecaric.Authentication.Service/Startup.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Apothecaric.Authentication.Service.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -31,6 +28,7 @@ namespace Apothecaric.Authentication.Service
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapGrpcService<AuthenticationService>();
 
                 endpoints.MapGet("/", async context =>
                 {
